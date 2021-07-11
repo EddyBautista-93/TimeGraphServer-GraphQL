3dff484bb6d3860f269e7ebe4ba6018ee0a8a6ea
EddyBautista <bautista.j.eddy@gmail.com>
2021-07-11 15:40:38 -0500

    added db class

---
+++ DataBase/TimeGraphContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeGraphServer.Models;
+
+namespace TimeGraphServer.DataBase
+{
+    // A dbcontext instance represents a combination of the unit of work and 
+    // repository patterns such that it can query from a databse.
+
+
+    public class TimeGraphContext : DbContext
+    {
+        public TimeGraphContext(DbContextOptions<TimeGraphContext> options) : base(options)
+        {
+        }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimeLog> TimeLogs { get; set; }
+    }
+}
